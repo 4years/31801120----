7f15cb76814525ee506b5de04e4cb8ce6cb32cf1
小梅 <48281988+4years@users.noreply.github.com>
2020-07-05 22:27:53 +0800

    Create MMerchantManager.java

---
+++ xm.takeway.control/MMerchantManager.java
@@ -0,0 +1,150 @@
+package xm.takeway.control;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import xm.takeway.itf.MerchantManager;
+import xm.takeway.model.BeanMerchant;
+import xm.takeway.util.BaseException;
+import xm.takeway.util.BusinessException;
+import xm.takeway.util.DBUtil;
+import xm.takeway.util.DbException;
+
+public class MMerchantManager implements MerchantManager {
+	public BeanMerchant reg(String merchantName,String merchantRank,int avg_consume,int total_sales,String pwd,String pwd2) throws BaseException {
+		Connection conn = null;
+		if("".equals(merchantName) || merchantName == null)
+			throw new BusinessException("商家名不能为空");
+		if("".equals(pwd) || pwd == null || "".equals(pwd2) || pwd2 == null)
+			throw new BusinessException("密码不能为空");
+		if(!(pwd.equals(pwd2)))
+			throw new BusinessException("两次密码输入需一致");
+		BeanMerchant BM = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "select merchant_id from merchant_message where merchant_name = ?";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, merchantName);
+			java.sql.ResultSet rs = pst.executeQuery();
+			if(rs.next())
+				throw new BusinessException("该商家名已存在");
+			rs.close();
+			pst.close();
+			
+			sql = "insert into merchant_message(merchant_name,merchant_pwd,merchant_rank,avg_consume,total_sales) values(?,?,?,?,?)";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, merchantName);
+			pst.setString(2, pwd);
+			pst.setString(3, merchantRank);
+			pst.setDouble(4, avg_consume);
+			pst.setInt(5, total_sales);
+			pst.execute();
+			pst.close();
+			conn.commit();
+			BM = new BeanMerchant();
+			BM.setMerchant_name(merchantName);
+			BM.setMerchant_pwd(pwd);
+			BM.setMerchant_rank(merchantRank);
+			BM.setAvg_consume(avg_consume);
+			BM.setTotal_sales(total_sales);
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null)
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return BM;
+	}
+	
+	public BeanMerchant login(String merchantName,String pwd) throws BaseException {
+		Connection conn = null;
+		if(merchantName == null || "".equals(merchantName))
+			throw new BusinessException("用户名不能为空");
+		if(pwd == null || "".equals(pwd))
+			throw new BusinessException("密码不能为空");
+		BeanMerchant BM = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "select merchant_pwd from merchant_message where merchant_name = ?";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, merchantName);
+			java.sql.ResultSet rs = pst.executeQuery();
+			if(rs.next()) {
+				if(!pwd.equals(rs.getString(1)))
+					throw new BusinessException("密码错误");
+				else {
+					BM = new BeanMerchant();
+					BM.setMerchant_name(merchantName);
+					BM.setMerchant_pwd(pwd);
+				}
+			}
+			else
+				throw new BusinessException("该用户不存在");
+			rs.close();
+			pst.close();
+			conn.commit();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null)
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return BM;
+	}
+	
+	public void changpwd(BeanMerchant merchant,String oldpwd,String newpwd,String newpwd2) throws BaseException {
+		if("".equals(newpwd) || newpwd == null || "".equals(newpwd2) || newpwd2 == null)
+			throw new BusinessException("新密码不能为空");
+		if(!(newpwd.equals(newpwd2)))
+			throw new BusinessException("两次密码输入不一致");
+		if(oldpwd.equals(newpwd))
+			throw new BusinessException("新密码不可与原密码一致");
+		Connection conn = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "select merchant_pwd from merchant_message where merchant_name = ?";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, merchant.getMerchant_name());
+			java.sql.ResultSet rs = pst.executeQuery();
+			rs.next();
+			if(!(oldpwd.equals(rs.getString(1))))
+				throw new BusinessException("原密码错误");
+			rs.close();
+			pst.close();
+			sql = "update merchant_message set merchant_pwd = ? where merchant_name = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, newpwd);
+			pst.setString(2, merchant.getMerchant_name());
+			pst.execute();
+			conn.commit();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null)
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+}
