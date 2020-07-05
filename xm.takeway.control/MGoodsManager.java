6f8c6b2416ebfe4d3a3007d4a70da0a8288c0217
小梅 <48281988+4years@users.noreply.github.com>
2020-07-05 22:31:04 +0800

    Create MGoodsManager.java

---
+++ xm.takeway.control/MGoodsManager.java
@@ -0,0 +1,162 @@
+package xm.takeway.control;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import xm.takeway.itf.GoodsManager;
+import xm.takeway.model.BeanGoodsDetails;
+import xm.takeway.model.BeanMerchant;
+import xm.takeway.util.BaseException;
+import xm.takeway.util.BusinessException;
+import xm.takeway.util.DBUtil;
+import xm.takeway.util.DbException;
+
+public class MGoodsManager implements GoodsManager {
+	public BeanGoodsDetails addGoods(int kind_id,String goods_name,double goods_price,double goods_sales,int goods_num) throws BaseException {
+		Connection conn = null;
+		if(goods_name == null || "".equals(goods_name))
+			throw new BusinessException("商品名不能为空");
+		BeanGoodsDetails BGD = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "select kind_name from merchant_goodskind where kind_id = ?";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, kind_id);
+			java.sql.ResultSet rs = pst.executeQuery();
+			if(!rs.next())
+				throw new BusinessException("该类别不存在");
+			rs.close();
+			pst.close();
+			sql = "select goods_id from merchant_goodsDetails where goods_name = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, goods_name);
+			rs = pst.executeQuery();
+			if(rs.next())
+				throw new BusinessException("同名商品已存在");
+			rs.close();
+			pst.close();
+			int order_id;
+			sql = "select count(*) from merchant_goodsDetails where merchant_name = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, BeanMerchant.currentLoginMerchant.getMerchant_name());
+			rs = pst.executeQuery();
+			if(rs.next())
+				order_id = rs.getInt(1) + 1;
+			else
+				order_id = 1;
+			sql = "insert into merchant_goodsDetails(kind_id,merchant_name,goods_name,goods_price,goods_sales,goods_num,order_id) values(?,?,?,?,?,?,?)";
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, kind_id);
+			pst.setString(2, BeanMerchant.currentLoginMerchant.getMerchant_name());
+			pst.setString(3, goods_name);
+			pst.setDouble(4, goods_price);
+			pst.setDouble(5, goods_sales);
+			pst.setInt(6, goods_num);
+			pst.setInt(7, order_id);
+			pst.execute();
+			pst.close();
+			conn.commit();
+			JOptionPane.showMessageDialog(null, "添加成功");
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null)
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return BGD;
+	}
+	
+	public List<BeanGoodsDetails> loadAll() throws BaseException {
+		List<BeanGoodsDetails> result = new ArrayList<BeanGoodsDetails>();
+		BeanGoodsDetails BGD = null;
+		Connection conn = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "select goods_id,kind_id,merchant_name,goods_name,goods_price,goods_sales,goods_num,order_id from merchant_goodsDetails where merchant_name = ? order by goods_id";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, BeanMerchant.currentLoginMerchant.getMerchant_name());
+			java.sql.ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				BGD = new BeanGoodsDetails();
+				BGD.setGoods_id(rs.getInt(1));
+				BGD.setKind_id(rs.getInt(2));
+				BGD.setMerchant_Name(rs.getString(3));
+				BGD.setGoods_name(rs.getString(4));
+				BGD.setGoods_price(rs.getDouble(5));
+				BGD.setGoods_sales(rs.getDouble(6));
+				BGD.setGoods_num(rs.getInt(7));
+				BGD.setOrder_id(rs.getInt(8));
+				result.add(BGD);
+			}
+			rs.close();
+			pst.close();
+			conn.commit();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null) 
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return result;
+	}
+	
+	public void delGoods(BeanGoodsDetails goodsdetails) throws BaseException {
+		if(goodsdetails.getGoods_num() != 0)
+			throw new BusinessException("该商品还有余量");
+		Connection conn = null;
+		try {
+			conn = DBUtil.getConnection();
+			conn.setAutoCommit(false);
+			String sql = "delete from merchant_goodsDetails where goods_id = ?";
+			java.sql.PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, goodsdetails.getGoods_id());
+			pst.execute();
+			pst.close();
+			
+			sql = "update merchant_goodsDetails set order_id = - order_id where merchant_name = ? and order_id > ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, goodsdetails.getMerchant_Name());
+			pst.setInt(2, goodsdetails.getOrder_id());
+			pst.execute();
+			pst.close();
+			
+			sql = "update merchant_goodsDetails set order_id = -1 - order_id where merchant_name = ? and order_id < 0";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, goodsdetails.getMerchant_Name());
+			pst.execute();
+			pst.close();
+					
+			conn.commit();
+			JOptionPane.showMessageDialog(null, "删除成功");
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		} finally {
+			if(conn != null)
+				try {
+					conn.rollback();
+					conn.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+}
