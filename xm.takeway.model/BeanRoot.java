0feb3d3a68d89063cd7845e47b8093a6275b5277
小梅 <48281988+4years@users.noreply.github.com>
2020-07-06 21:20:09 +0800

    Create BeanRoot.java

---
+++ xm.takeway.model/BeanRoot.java
@@ -0,0 +1,20 @@
+package xm.takeway.model;
+
+public class BeanRoot {
+	private String root_name;
+	private String root_pwd;
+	
+	public String getRoot_pwd() {
+		return root_pwd;
+	}
+	public void setRoot_pwd(String root_pwd) {
+		this.root_pwd = root_pwd;
+	}
+	public String getRoot_name() {
+		return root_name;
+	}
+	public void setRoot_name(String root_name) {
+		this.root_name = root_name;
+	}
+	
+}
