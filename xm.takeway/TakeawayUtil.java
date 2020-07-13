896e4469874578afab5547cc3e91bcd944e6b026
小梅 <48281988+4years@users.noreply.github.com>
2020-07-13 13:47:52 +0800

    Add files via upload

--- xm.takeway/TakeawayUtil.java
+++ xm.takeway/TakeawayUtil.java
@@ -4,6 +4,7 @@ import xm.takeway.control.MCouponManager;
 import xm.takeway.control.MGoodsManager;
 import xm.takeway.control.MKnightManager;
 import xm.takeway.control.MMerchantManager;
+import xm.takeway.control.MOrderManager;
 import xm.takeway.control.MRootManager;
 import xm.takeway.control.MShoppingCar;
 import xm.takeway.control.MUserManager;
@@ -11,6 +12,7 @@ import xm.takeway.itf.CouponManager;
 import xm.takeway.itf.GoodsManager;
 import xm.takeway.itf.KnightManager;
 import xm.takeway.itf.MerchantManager;
+import xm.takeway.itf.OrderManager;
 import xm.takeway.itf.RootManager;
 import xm.takeway.itf.ShoppingCarManager;
 import xm.takeway.itf.UserManager;
@@ -23,4 +25,5 @@ public class TakeawayUtil {
 	public static KnightManager knightManager = new MKnightManager();
 	public static CouponManager couponManager = new MCouponManager();
 	public static RootManager rootManager = new MRootManager();
+	public static OrderManager orderManager = new MOrderManager();
 }
